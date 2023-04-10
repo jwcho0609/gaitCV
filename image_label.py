e2a6d42cf5e2b4c2d0899b8e52f4607614da8f13
jwcho0609 <jwcho0609@gmail.com>
2023-04-10 15:46:48 -0400

    labeling refactor

--- image_label.py
+++ image_label.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 import torch
 import torchvision.transforms as T 
 
-from utils.post_processing import filter_depth, approx_bounding_box, perform_grabcut, visualize_boundbox
+from utils.post_processing import segment, cleanContours, findBoundingBox
+from utils.data_org import load_img
 
 
 CAMERA_ORI = "horizontal"
@@ -22,114 +23,22 @@ N = int((len([name for name in os.listdir(path) if os.path.isfile(os.path.join(p
 fcn = models.segmentation.fcn_resnet101(weights=models.segmentation.FCN_ResNet101_Weights.DEFAULT).eval()
 dlab = models.segmentation.deeplabv3_resnet101(weights=models.segmentation.DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1).eval()
 
-
-def load_img(path):
-    img_rgb = Image.open(path)
-
-    if CAMERA_ORI == 'vertical':
-        img_rgb = img_rgb.rotate(90, Image.NEAREST, expand = 1)
-    img_np = np.array(img_rgb.convert('RGB'))
-    img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
-
-    return img_rgb, img_np
-
-
-# Define the helper function
-def decode_segmap(image, nc=21):
-    label_colors = np.array([(0, 0, 0),  # 0=background
-               # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
-    r = np.zeros_like(image).astype(np.uint8)
-    g = np.zeros_like(image).astype(np.uint8)
-    b = np.zeros_like(image).astype(np.uint8)
-    for l in range(0, nc):
-        if l == 15:
-            idx = image == l
-            r[idx] = label_colors[l, 0]
-            g[idx] = label_colors[l, 1]
-            b[idx] = label_colors[l, 2]
-            rgb = np.stack([r, g, b], axis=2)
-    return rgb
-
-
-def segment(img_rgb, img_np, net, visual=False):
-    # Comment the Resize and CenterCrop for better inference results
-    trf = T.Compose([
-                    # T.Resize(256), 
-                    # T.CenterCrop(224), 
-                    T.ToTensor(), 
-                    T.Normalize(mean = [0.485, 0.456, 0.406], 
-                                std = [0.229, 0.224, 0.225])])
-    inp = trf(img_rgb).unsqueeze(0)
-    out = net(inp)['out']
-    om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
-    mask_rgb = decode_segmap(om)
-
-    # convert our RGB mask into an image for PDF report generation
-    mask_rgb = Image.fromarray(mask_rgb.astype('uint8'),'RGB')
-    
-    # binary mask conversion
-    mask_b = np.sum(mask_rgb, axis=2).astype(np.uint8)
-    mask_b[mask_b > 0] = 1
-
-    if visual:
-        cv2.imshow('original image', img_np)
-        cv2.imshow('mask', mask_b*255)
-        cv2.waitKey(0)
-
-    return mask_b, mask_rgb
-
-
-def cleanContours(img_np, mask, min_area=1000, visual=False):
-
-    # find only the external contours in the mask
-    cnts, h = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print("Number of Contours found = " + str(len(cnts)))
-
-    # stacked mask to comply for RGB image dimensions: (N,M,3)
-    mask_stack = np.dstack((mask,mask,mask))*255
-
-    # iterate through each contour, and remove those that are less than a specified size
-    for c in cnts:
-        if cv2.contourArea(c) < min_area:
-            mask_stack = cv2.drawContours(mask_stack, [c], -1, (0,0,0), -1)
-        else:
-            mask_stack = cv2.drawContours(mask_stack, [c], -1, (255,255,255), -1)
-
-    # refind our final contours
-    cnts, h = cv2.findContours(mask_stack[:,:,0], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # visualization of final contours on image
-    if visual:
-        img = img_np.copy()
-        img = cv2.drawContours(img, cnts, -1, (0,255,0), 2)
-
-        cv2.imshow('Image with final contours', img)
-        cv2.waitKey(0)
-
-    return cnts, h
-
-
-def findBoundingBox(mask):
-    return None
-
-
+# final image list for report generation purposes
 im_list = []
 
 for i in np.arange(10):
-    print(f'processing image: {i}')
-    path = f'./data/{CAMERA_ORI}/{SHOE_FACTOR}/color_{i}.jpeg'
+    path = f'data/{CAMERA_ORI}/{SHOE_FACTOR}/color_{i}.jpeg'
+    print(f'processing image: {path}')
     
-    img_rgb, img_np = load_img(path)
+    img_rgb, img_np = load_img(path, CAMERA_ORI)
 
-    mask_b, mask_rgb = segment(img_rgb, img_np, dlab, visual=True)
-    cnt, _ = cleanContours(img_np, mask_b, visual=True)
+    mask_b, mask_rgb = segment(img_rgb, img_np, dlab, visual=False)
+    mask_b, img_cnt = cleanContours(img_np, mask_b, cnt_approx=False, visual=False)
+    bbs,img_bb = findBoundingBox(img_cnt, mask_b)
+
+    cv2.imshow("original image", img_np)
+    cv2.imshow("image with bounding box and contour", img_bb)
+    cv2.waitKey(0)
 
     im_list.append(img_rgb)
 
