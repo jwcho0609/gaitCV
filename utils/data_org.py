9f291aaf944fc345320f2cddadef44fea4c57247
jwcho0609 <jwcho0609@gmail.com>
2023-04-10 15:46:55 -0400

    load image

--- utils/data_org.py
+++ utils/data_org.py
@@ -1,8 +1,21 @@
 import numpy as np
 import cv2
+from PIL import Image
+from pathlib import Path
 
 def numpy_to_jpeg(path, n):
     for i in np.arange(n):
         color_image = np.load(f"{path}/color_{i}.npy")
         cv2.imwrite(f"{path}/color_{i}.jpeg", color_image)
 
+
+def load_img(path, ori):
+    parent_dir = Path().resolve()
+    img_rgb = Image.open(parent_dir/path)
+
+    if ori == 'vertical':
+        img_rgb = img_rgb.rotate(90, Image.NEAREST, expand = 1)
+    img_np = np.array(img_rgb.convert('RGB'))
+    img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+
+    return img_rgb, img_np
